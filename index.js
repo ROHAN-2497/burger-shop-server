faf65423edc769f3824d6c68f7c3c5867df7f57f
rohan future devoloper <xrohaislam373@gmail.com>
2023-12-14 22:07:53 +0600

    cartsCollection

--- index.js
+++ index.js
@@ -28,24 +28,33 @@ async function run() {
     const menuCollection = client.db("burgerShop").collection("menu");
     const reviewCollection = client.db("burgerShop").collection("review");
     const cartCollection = client.db("burgerShop").collection("carts");
-    // menuCollecion
+    // menuCollecion api
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
     });
 
-    // reviewCollection
+    // reviewCollection api
     app.get("/review", async (req, res) => {
       const result = await reviewCollection.find().toArray();
       res.send(result);
     });
-    // cartCollection
-    app.post('/carts', async (req, res) =>{
+    // cartCollection api
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
+        res.send([]);
+      }
+      const query = { email: email };
+      const result = await cartCollection.find("carts").toArray();
+      res.send(result);
+    });
+    app.post("/carts", async (req, res) => {
       const item = req.body;
       console.log(item);
       const result = await cartCollection.insertOne(item);
       res.send(result);
-    })
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
